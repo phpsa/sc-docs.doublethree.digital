c15556fc464de534b8f28a10e73a41555b910875
Duncan McClean <duncan@mcclean.co.uk>
2020-10-14 20:35:10 +0100

    Redirect to current version docs

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::statamic('search', 'search');
+
+Route::name('current_release_redirects')->group(function () {
+	$currentSite = \Statamic\Facades\Site::get('v2.1');
+
+	Route::redirect('/', $currentSite->url());
+	Route::redirect('/installation', $currentSite->url().'/installation');
+	Route::redirect('/configuring', $currentSite->url().'/configuring');
+	Route::redirect('/gateways', $currentSite->url().'/gateways');
+	Route::redirect('/shipping', $currentSite->url().'/shipping');
+	Route::redirect('/tags', $currentSite->url().'/tags');
+	Route::redirect('/email', $currentSite->url().'/email');
+	Route::redirect('/how-it-works', $currentSite->url().'/how-it-works');
+	Route::redirect('/knowledge-base/version-control-strategies', $currentSite->url().'/knowledge-base/version-control-strategies');
+	Route::redirect('/extending', $currentSite->url().'/how-it-works');
+});
