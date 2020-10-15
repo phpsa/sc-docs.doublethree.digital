2cc32f5ef9fb8a1d447f7f1ffc41bc69b2b6addf
Duncan McClean <duncan@mcclean.co.uk>
2020-10-15 12:58:36 +0100

    cleanup layout

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,8 @@
 require('./prism')
+
+// Table of Contents
+document.addEventListener('scroll', function () {
+    let tocItems = Array.from(document.getElementsByClassName('toc')[0].children).map((child) => {
+        return document.getElementById(child.children[0].id.replace('anchor-', ''))
+    })
+})
