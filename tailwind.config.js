f26fa2bd98ee0f625193b4ffac19c0e775cb889b
Duncan McClean <duncan@mcclean.co.uk>
2021-02-06 09:47:20 +0000

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ let defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
     purge: {
         content: [
-            './app/*/*.php'
+            './app/*/*.php',
             './resources/**/*.antlers.html',
             './resources/**/*/*.antlers.html',
             './content/**/*.md'
