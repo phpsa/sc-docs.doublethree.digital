724ba454b66c51d0074cf238ec84808857df05d5
Duncan McClean <duncan@mcclean.co.uk>
2021-02-09 21:14:44 +0000

    wip

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,8 +1,1 @@
 require('./prism')
-
-// Table of Contents
-document.addEventListener('scroll', function () {
-    Array.from(document.getElementsByClassName('toc')[0].children).map((child) => {
-        return document.getElementById(child.children[0].id.replace('anchor-', ''))
-    })
-})
