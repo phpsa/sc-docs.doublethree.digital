f7f0b9243ca948453714547e842e211d1bd5a4a7
Duncan McClean <duncan@mcclean.co.uk>
2021-02-26 17:28:29 +0000

    wip

---
+++ app/Http/Controllers/SearchFeedbackController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchFeedbackController extends Controller
+{
+    public function store(Request $request)
+    {
+        $searchLogPath = storage_path('app/searches.json');
+
+        if (! file_exists($searchLogPath)) {
+            file_put_contents($searchLogPath, json_encode([]));
+        }
+
+        $log = collect(json_decode(file_get_contents($searchLogPath), true))
+            ->merge([
+                $request->searchQuery,
+            ])
+            ->unique()
+            ->toJson(JSON_PRETTY_PRINT);
+
+        file_put_contents($searchLogPath, $log);
+
+        return [
+            'message' => "What a sunny day outside ☀️"
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,79 +1,32 @@
 <?php
 
 use App\Http\Controllers\ChangelogController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\SearchFeedbackController;
 use Illuminate\Support\Facades\Route;
 use Statamic\Facades\Site;
 use Statamic\Statamic;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::name('current_release_redirects')->group(function () {
-    $currentSite = array_first(config('statamic.sites.sites'));
-
-    Route::redirect('/', $currentSite['url']);
-    Route::redirect('/home', $currentSite['url']);
-	Route::redirect('/installation', $currentSite['url'].'installation');
-	Route::redirect('/configuring', $currentSite['url'].'configuring');
-	Route::redirect('/multi-site', $currentSite['url'].'multi-site');
-	Route::redirect('/gateways', $currentSite['url'].'gateways');
-	Route::redirect('/shipping', $currentSite['url'].'shipping');
-	Route::redirect('/tags', $currentSite['url'].'tags');
-	Route::redirect('/email', $currentSite['url'].'email');
-	Route::redirect('/how-it-works', $currentSite['url'].'how-it-works');
-	Route::redirect('/knowledge-base/version-control-strategies', $currentSite['url'].'knowledge-base/version-control-strategies');
-	Route::redirect('/extending', $currentSite['url'].'how-it-works');
-	Route::redirect('/product-variants', $currentSite['url'].'product-variants');
-});
-
 Statamic::booted(function () {
-    foreach (Site::all() as $site) {
-        // Route::statamic($site->url().'/search', 'search', [
-        //     'title' => 'Search results',
-        //     'site' => $site,
-        // ]);
+    Route::name('latest-version.')->group(function () {
+        $latestVersion = Site::get('v2.2');
 
-        Route::get($site->url().'/changelog', [ChangelogController::class, 'show']);
+        Route::redirect('/{any}', url($latestVersion->url(), request()->segment(1)), 301);
+        Route::redirect('/tags/{any}', url($latestVersion->url(), request()->segment(1)), 301);
+    });
+
+    foreach (Site::all() as $site) {
+        Route::name($site->handle().'.')->prefix($site->handle())->group(function () {
+            Route::get('/changelog', [ChangelogController::class, 'show'])->name('changelog');
+        });
     }
 });
 
 Route::redirect('/github', 'https://github.com/doublethreedigital/simple-commerce');
+Route::redirect('/discord', 'https://github.com/doublethreedigital/simple-commerce/discussions');
+
+Route::post('/search-feedback', [SearchFeedbackController::class, 'store']);
 
 Route::statamic('/sitemap.xml', 'sitemap', [
     'layout' => null,
     'content_type' => 'xml',
 ]);
-
-/**
- * This route will log search queries made in the
- * documentation, so popular requests are documented.
- */
-Route::post('/search-feedback', function (Request $request) {
-    $searchLogPath = storage_path('app/searches.json');
-
-    if (! file_exists($searchLogPath)) {
-        file_put_contents($searchLogPath, json_encode([]));
-    }
-
-    $log = collect(json_decode(file_get_contents($searchLogPath), true))
-        ->merge([
-            $request->searchQuery,
-        ])
-        ->unique()
-        ->toJson(JSON_PRETTY_PRINT);
-
-    file_put_contents($searchLogPath, $log);
-
-    return [
-        'message' => "What a sunny day outside..."
-    ];
-});
